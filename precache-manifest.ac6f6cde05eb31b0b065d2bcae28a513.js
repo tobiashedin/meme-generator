0e2f3b655ff6c50b74f5e44962ad0d960a1c24c7
unknown <tobias.hedin@gmail.com>
2019-05-09 09:57:23 +0200

    Updates

---
+++ precache-manifest.ac6f6cde05eb31b0b065d2bcae28a513.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d904cea0814c647d627e8ca2f8a3c6",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/meme-generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "b9d1b254d2c4fc67d209",
+    "url": "/meme-generator/static/js/main.14683612.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  }
+]);
